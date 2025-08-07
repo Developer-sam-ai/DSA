0ae354cb26c6339fbfd7b69e87f09d5ec70a5a35
sam <sam.developer31@gmail.com>
2025-08-07 10:30:19 +0530

    strated with day 1
    things we want to carry as started with using eclipse for DSA something new

---
+++ Basics_and_more/recursion_1.java
@@ -0,0 +1,25 @@
+package Basics_and_more;
+
+public class recursion_1 {
+	public static int sum=1;
+	public static void main(String...args) {
+		
+		NnumbersSum(5);
+		System.out.println(sum);
+	}
+	public static int NnumbersSum(int N) {
+        if(N>1){
+        sumofn(N);
+        }
+        return 0;
+    }
+    public static int sumofn(int N){
+    	if(N>1) {
+        sum+=N;
+        return sumofn(N-1);
+        }
+    	else {
+    		return 0;
+    	}
+    }
+}
