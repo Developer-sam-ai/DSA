eadaf3644c99ad7d15f695c74b1a00889a8d4f6c
sam <sam.developer31@gmail.com>
2025-08-15 19:48:18 +0530

    general

--- concepts/solve_10_seven_problem.java
+++ concepts/solve_10_seven_problem.java
@@ -8,9 +8,29 @@ public class solve_10_seven_problem {
 //		h1[arr[i]]++;
 //	}
 	
-	for(char heel:arr) {
-		h1.put(heel, h1.getOrDefault(heel,0)+1);
+//	for(char heel:arr) {
+//		h1.put(heel, h1.getOrDefault(heel,0)+1);
+		
+//		h1.put(heel, h1.getOrDefault(0,0)+1);
+		//out-put
+//		{s=1, c=1, u=1, f=1, g=1, y=1}
+//		if we add 0 to 0 then it will just add to zero as a default value
+//	}
+	
+	// other m to try on
+	for(char r:arr) {
+		if(h1.containsKey(r)) {
+			h1.put(r,h1.get(r)+1);
+		}
+		else {
+			h1.put(r, 1);
+		}
 	}
+	
+    // other method 
+//	for(char s:arr) {
+//		h1.compute(s,(key,val)->(val==null)? 1:val+1);
+//	}
 	System.out.println(h1);
 	}
 }
