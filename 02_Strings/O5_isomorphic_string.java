e6557f99953006da8b24deb584f6db9ae5b5d813
sam <sam.developer31@gmail.com>
2025-11-18 17:44:34 +0530

    isomorphic string

--- 02_Strings/O5_isomorphic_string.java
+++ 02_Strings/O5_isomorphic_string.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
 public class O5_isomorphic_string {
     public static void main(String[] args) {
         String s="paper";
-        String t="foler";
+        String t="title";
         // we will try it by hashmap 
         HashMap<Character,Character>map=new HashMap<>();
         HashSet<Character>used=new HashSet<>();
@@ -17,12 +17,23 @@ public class O5_isomorphic_string {
             char a=s.charAt(i);
             char b=t.charAt(i);
             if(map.containsKey(a)){
-                System.out.println("true");
-            }
+            if(map.get(a)!=b){
+                    System.out.println("false");
+                    return;
+                }
+            else{
             if(used.contains(b)){
             System.out.println("false");
+            return ;
             }
         }
-
+            map.put(a,b);
+            used.add(b);
+    }
+        }
+        System.out.println("true");
     }
 }
+
+// ! just something new in this questions where we map every charachter and we make hashmap of charcter itself not string 
+//? we are checking and keeping reocrd of the used ones in the used 
