b3dc2914363ac2cbd8f80ca7c3539fb58a819daa
sam <sam.developer31@gmail.com>
2025-11-25 19:52:31 +0530

    add head

--- 03_Linked_List/O1_insert_At_Head.java
+++ 03_Linked_List/O1_insert_At_Head.java
@@ -1,41 +1,87 @@
 
-    class Node{
-        int data;
-        Node next;
-    
-        Node(int data) {
-            this.data=data;
-            this.next=null;
-        }
-    }
-    class LinkedList{
-        Node head;
+// import java.lang.classfile.components.ClassPrinter;
 
-        public void InsertHead(int value){
+
+//     class Node{
+//         int data;
+//         Node next;
+    
+//         Node(int data) {
+//             this.data=data;
+//             this.next=null;
+//         }
+//     }
+//     class LinkedList{
+//         Node head;
+
+//         public void InsertHead(int value){
+//             Node newnode=new Node(value);
+//             newnode.next=head;
+//             head=newnode;
+//         }
+    
+//     public void printlist(){
+//         Node temp=head;
+//         while(temp!=null){
+//             System.out.print(temp.data+"->");
+//             temp=temp.next;
+//         }
+//         System.out.println("null");
+//     }
+// }
+
+
+// public class O1_insert_At_Head {
+//     public static void main(String[] args) {
+//         LinkedList list=new LinkedList();
+
+//         list.InsertHead(10);
+//         list.InsertHead(20);
+//         list.InsertHead(30);
+
+//         list.printlist();
+//     }
+// }
+
+
+
+
+
+
+
+class Node{
+    int value;
+    Node next;
+    Node(int valu){
+        this.value=valu;
+        this.next=null;
+    }
+
+}
+class LinkedList{
+    Node head;
+        public void insertNode(int value){
             Node newnode=new Node(value);
             newnode.next=head;
             head=newnode;
         }
-    
-    public void printlist(){
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.data+"->");
-            temp=temp.next;
+        public void printList(){
+                Node temp=head;
+                while(temp!=null){
+                    System.out.print(temp.value+"->");
+                    temp=temp.next;
+                }
+
         }
-        System.out.println("null");
-    }
 }
-
-
-public class O1_insert_At_Head {
+public class O1_insert_At_Head{
     public static void main(String[] args) {
-        LinkedList list=new LinkedList();
+        LinkedList newlist=new LinkedList();
+        newlist.insertNode(45);
+        newlist.insertNode(34);
+        newlist.insertNode(67);
+        newlist.insertNode(89);
 
-        list.InsertHead(10);
-        list.InsertHead(20);
-        list.InsertHead(30);
-
-        list.printlist();
+        newlist.printList();
     }
 }
