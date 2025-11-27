336d984d1b6b2190db0b48f627416180ca1f9a95
sam <sam.developer31@gmail.com>
2025-11-27 22:32:28 +0530

    reversed try

---
+++ 03_Linked_List/01_Singly linked_List/Reverse_the_LinkedList.java
@@ -0,0 +1,12 @@
+public class Reverse_the_LinkedList {
+    public static void main(String...args){
+        ListNode temp=head;
+        while(temp!=null){
+            temp=temp.next;
+        }
+        Listnode back=temp;
+        Listnode front=head;
+
+        while(
+    }
+}
