086f6dae361899bc5d98285fac702df8ae38f61a
sam <sam.developer31@gmail.com>
2026-01-06 17:53:22 +0530

    Add next smaller element algorithm implementation
    Implemented a function to find the next smaller element for each element in an array using a stack. Added a main method to demonstrate the algorithm and print the results.

--- 05_Stack/03_leet_stack/O3_Next_Smaller_element.java
+++ 05_Stack/03_leet_stack/O3_Next_Smaller_element.java
@@ -1,3 +1,29 @@
+
+import java.util.Arrays;
+import java.util.Stack;
+
 public class O3_Next_Smaller_element {
-    
+    public static void main(String[] args) {
+        int arr[]={4,8,5,2,25};
+        int res[]=new int[arr.length];
+        Stack<Integer> st=new Stack<>();
+
+        int n=arr.length;
+        for(int i=n-1;i>=0;i--){
+            int current=arr[i];
+            while(!st.isEmpty() && st.peek()>=current){
+                st.pop();
+
+            }
+            if(st.isEmpty()){
+                res[i]=-1;
+            }
+            else{
+                res[i]=st.peek();
+                
+            }
+            st.push(current);
+        }
+        System.out.println(Arrays.toString(res));
+    }
 }
