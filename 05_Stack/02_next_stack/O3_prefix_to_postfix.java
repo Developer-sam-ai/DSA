4148fe72efe692eee5708f7bed6af330ebcb8ccd
sam <sam.developer31@gmail.com>
2026-01-06 12:48:03 +0530

    Add prefix to postfix conversion implementation
    Implemented logic in O3_prefix_to_postfix.java to convert a prefix expression to postfix using a stack. Updated .vscode/settings.json to include 'stri' in word patterns.

--- 05_Stack/02_next_stack/O3_prefix_to_postfix.java
+++ 05_Stack/02_next_stack/O3_prefix_to_postfix.java
@@ -1,5 +1,24 @@
+
+import java.util.Stack;
+
 public class O3_prefix_to_postfix {
     public static void main(String[] args) {
-        
+        String stri="+ab-cd";
+        Stack<String> st=new Stack<>();
+        for(int i=stri.length()-1;i>=0;i--){
+            char current=stri.charAt(i);
+            if(Character.isAlphabetic(current)){
+                st.push(current+"");
+            }
+            else{
+                    String result=st.pop()+st.pop()+current;
+                    st.push(result);
+            }
+        }
+        String result="";
+        while(!st.isEmpty()){
+            result+=st.pop();
+        }
+        System.out.println(result);
     }
 }
